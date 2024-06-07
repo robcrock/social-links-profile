f612b9805e0c07f73c1567d89c90462e453c6725
Robert Crocker <rcrocker13@gmail.com>
2024-06-07 10:46:09 -0500

    Updated styles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        Jessica Randall London, United Kingdom Front-end developer and avid
-        reader. GitHub Frontend Mentor LinkedIn Twitter Instagram
-      </p>
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
+    <main className="flex min-h-screen flex-col items-center justify-between bg-black p-24">
+      <div className="bg-offBlack h-[611px] w-[384px]">
+        <p>
+          Jessica Randall London, United Kingdom Front-end developer and avid
+          reader. GitHub Frontend Mentor LinkedIn Twitter Instagram
+        </p>
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,30 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#141414",
+      offBlack: "#1f1f1f",
+      grey: "#333333",
+      white: "#ffffff",
+      green: "#c4f82a",
+    },
+    fontSize: {
+      base: "0.875rem",
+      lg: "1.5rem",
+    },
+    fontFamily: {
+      inter: "Inter",
+    },
+    borderRadius: {
+      "rounded-0": "0rem",
+      "rounded-1": "0.5714285714285714rem",
+      "rounded-2": "0.8571428571428571rem",
+      "rounded-3": "71.35714285714286rem",
+    },
+    screens: {
+      m: "375px",
+      d: "1440px",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-inter)"],
